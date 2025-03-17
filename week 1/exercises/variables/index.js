f7152dfcf991381e115887240fb5e5e8d737ab52
codeDaydreamer <codedaydreamer@gmail.com>
2025-03-17 12:00:24 +0300

    edited structure

---
+++ week 1/exercises/variables/index.js
@@ -0,0 +1,20 @@
+document.getElementById(`myH1`).textContent = `Hello World!`;
+document.getElementById(`myP`).textContent = `I like pizza!`;
+
+
+let x;
+x = 100;
+
+let y = 200;
+let age = 20;
+let firstName = `John`;
+
+
+console.log(y);
+console.log(x); 
+console.log(age);
+
+console.log(`you are ${age} years old`); // you are 20 years old
+console.log(typeof age); // number
+console.log(typeof firstName); // string
+console.log(`your name is ${firstName}`); // your name is John
